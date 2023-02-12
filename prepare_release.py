c3614ac39aadc3f4e018c0eb7c8f354a50c93c94
gael <gael.vanderlee@gmail.com>
2023-02-12 04:12:09 +0100

    Progress on mac compilation

--- prepare_release.py
+++ prepare_release.py
@@ -1,10 +1,12 @@
 # Prepare the release library
 import os
 import shutil
+from subprocess import Popen, PIPE
 
 
 def compile_clean(name_overlay: str, game_folder: str, out_lib_name: str,
-                  disable_console: bool = True, finalize_folder: bool = False):
+                  disable_console: bool = True, finalize_folder: bool = False,
+                  macos=False):
     """Compile an overlay program and clean the building files.
 
     Parameters
@@ -18,25 +20,31 @@ def compile_clean(name_overlay: str, game_folder: str, out_lib_name: str,
     icon = 'pictures/common/icon/salamander_sword_shield.ico'  # icon for the library
 
     # main nuitka command to run
-    main_command = ('cmd /c "python -m nuitka'
-                    ' --standalone'
-                    ' --plugin-enable=PyQt6'
-                    # ' --macos-create-app-bundle'
-                    f' --windows-icon-from-ico={icon}'
-                    f' --include-data-dir=common=common'
-                    f' --include-data-dir={game_folder}={game_folder}'
-                    f' --include-data-dir=pictures/common=pictures/common'
-                    f' --include-data-dir=pictures/{game_folder}=pictures/{game_folder}'
-                    f' --include-data-dir=build_orders/{game_folder}=build_orders/{game_folder}'
-                    f' --include-data-dir=audio=audio'
-                    )
+    if macos:
+        main_command = f"python -m nuitka --macos-create-app-bundle --macos-app-icon={icon}"
+    else:
+        main_command = ('cmd /c "python -m nuitka'
+                        ' --standalone'
+                        ' --plugin-enable=PyQt6'
+                        f' --windows-icon-from-ico={icon}'
+                        f' --include-data-dir=common=common'
+                        )
 
+    main_command = main_command + (
+        f" --output-dir={out_lib_name}"
+        f" --plugin-enable=pyqt6"
+        f' --include-data-dir={game_folder}={game_folder}'
+        f' --include-data-dir=pictures/common=pictures/common'
+        f' --include-data-dir=pictures/{game_folder}=pictures/{game_folder}'
+        f' --include-data-dir=build_orders/{game_folder}=build_orders/{game_folder}'
+        # f' --include-data-dir=audio=audio'
+    )
     if disable_console:  # disable the console
-        command = main_command + ' --windows-disable-console' + f' {name_overlay}.py'
+        command = main_command + f' --disable-console {name_overlay}.py'
     else:  # show the console
         command = main_command + f' {name_overlay}.py'
 
-    os.system(command)  # compilation
+    print(Popen(command, stdout=PIPE, shell=True).communicate()[0].decode())  # compilation
 
     # rename executable name for version with console
     if not disable_console:
@@ -44,11 +52,12 @@ def compile_clean(name_overlay: str, game_folder: str, out_lib_name: str,
                   os.path.join(f'{name_overlay}.dist', f'{name_overlay}_with_console.exe'))
 
     # copy files in output directory
-    shutil.copytree(f'{name_overlay}.dist', out_lib_name, dirs_exist_ok=True)
+    if not macos:
+        shutil.copytree(f'{out_lib_name}/{name_overlay}.dist', out_lib_name, dirs_exist_ok=True)
+        shutil.rmtree(f'{out_lib_name}/{name_overlay}.dist')
 
     # clean building files
-    shutil.rmtree(f'{name_overlay}.build')
-    shutil.rmtree(f'{name_overlay}.dist')
+    # shutil.rmtree(f'{out_lib_name}/{name_overlay}.build')
 
     # finalize the output folder
     if finalize_folder:
@@ -64,25 +73,28 @@ def compile_clean(name_overlay: str, game_folder: str, out_lib_name: str,
 
         # zip output folder
         shutil.make_archive(out_lib_name, 'zip', out_lib_name)
-        shutil.rmtree(out_lib_name)  # clean non-zipped files
+        # shutil.rmtree(out_lib_name)  # clean non-zipped files
 
 
 if __name__ == '__main__':
     # name of the output libraries
+    macos = True
     aoe2_library_name = 'aoe2_overlay'
     aoe4_library_name = 'aoe4_overlay'
+    shutil.rmtree(aoe2_library_name, ignore_errors=True)
+    shutil.rmtree(aoe4_library_name, ignore_errors=True)
     assert (not os.path.isdir(aoe2_library_name)) and (not os.path.isdir(aoe4_library_name))
 
     # Age of Empires II
     os.mkdir(aoe2_library_name)
+    # compile_clean(name_overlay='aoe2_overlay', game_folder='aoe2', out_lib_name=aoe2_library_name,
+    #               disable_console=False, finalize_folder=False, macos=macos)
     compile_clean(name_overlay='aoe2_overlay', game_folder='aoe2', out_lib_name=aoe2_library_name,
-                  disable_console=False, finalize_folder=False)
-    compile_clean(name_overlay='aoe2_overlay', game_folder='aoe2', out_lib_name=aoe2_library_name,
-                  disable_console=True, finalize_folder=True)
+                  disable_console=True, finalize_folder=True, macos=macos)
 
     # Age of Empires IV
-    os.mkdir(aoe4_library_name)
-    compile_clean(name_overlay='aoe4_overlay', game_folder='aoe4', out_lib_name=aoe4_library_name,
-                  disable_console=False, finalize_folder=False)
-    compile_clean(name_overlay='aoe4_overlay', game_folder='aoe4', out_lib_name=aoe4_library_name,
-                  disable_console=True, finalize_folder=True)
+    # os.mkdir(aoe4_library_name)
+    # compile_clean(name_overlay='aoe4_overlay', game_folder='aoe4', out_lib_name=aoe4_library_name,
+    #               disable_console=False, finalize_folder=False, macos=macos)
+    # compile_clean(name_overlay='aoe4_overlay', game_folder='aoe4', out_lib_name=aoe4_library_name,
+    #               disable_console=True, finalize_folder=True, macos=macos)
